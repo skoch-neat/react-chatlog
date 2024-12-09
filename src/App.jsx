35b49377594c13adaef4614171bcd36a88bd26d9
Sarah Koch <sarahkoch810@gmail.com>
2024-12-09 14:23:07 -0800

    Updated App component to render ChatEntry with data from the first message in the JSON file. Passed sender, body, and timeStamp props to the ChatEntry component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import messages from './data/messages.json';
 
 const App = () => {
+  const firstMessage = messages[0];
+
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={firstMessage.sender}
+          body={firstMessage.body}
+          timeStamp={firstMessage.timeStamp}
+        />
+        {/* Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
