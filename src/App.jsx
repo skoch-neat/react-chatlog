675ab57dcce9c8e209789126f6ea02cf624b4763
Sarah Koch <sarahkoch810@gmail.com>
2024-12-09 17:22:14 -0800

    Updated App component to render the ChatLog component with the messages data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import './App.css';
-import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog';
 import messages from './data/messages.json';
 
 const App = () => {
-  const firstMessage = messages[0];
-
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        <ChatEntry
-          sender={firstMessage.sender}
-          body={firstMessage.body}
-          timeStamp={firstMessage.timeStamp}
-        />
-        {/* Wave 02: Render ChatLog component */}
+        <ChatLog entries={messages} />
       </main>
     </div>
   );
