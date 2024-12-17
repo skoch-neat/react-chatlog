9a23a421b9fac810202ced333f2e0fab0bb7a2f6
Sarah Koch <sarahkoch810@gmail.com>
2024-12-16 16:15:34 -0800

    Fix homepage URL and update base path in Vite config for consistency

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-chatlog',
+  base: '/react-chatlog/',
   plugins: [react()],
   test: {
     // jest config here
